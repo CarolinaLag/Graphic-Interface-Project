01ff28fd913f02f3fcdd26ccfcc9353274896877
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-04-14 14:04:10 +0200

    animated all icons and added scss minify

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-// const btn = document.querySelector("#knapp");
-// btn.addEventListener('click', () => {
-//     gsap.fromTo('.box', {rotation: 0 }, {rotation: 360, duration: 5} );
-// });
 
 window.addEventListener('load', function() {
-    const tl = new gsap.timeline({ repeat: -1, repeatDelay: 2});
+   
+    TweenLite.to("#parasoll", 10, {rotation:1080, ease:Power3.easeInOut, repeat: -1})
 
-    // tl.fromTo('#parasoll', {y: -400}, {y:0, ease: Bounce.easeOut, duration: 1.2})
-    TweenLite.to("#parasoll", 10, {rotation:700, ease:Power3.easeInOut})
+    TweenMax.to("#boat", 1, {y:"20", repeat:10, yoyo:true, ease:Sine.easeInOut, repeat: -1});
+
+    TweenMax.to("#car", 1, {x:20, repeat:10, yoyo:true, ease:Sine.easeInOut, repeat: -1});
+
+    function Tween(){
+        let T = TweenLite.to("#house",0.15,{x:R(-10,10),y:R(-10,10),scale:R(1.1,0.9),rotation:R(-10,10),ease:Sine.easeInOut,onComplete:Tween})
+        };
+        
+        Tween();
+        
+        function R(max,min){return Math.random()*(max-min)+min};
 })
