6057bc22668875eb36bcadf910cbb6acc876c39f
Carolina Lagerberg <carolina.lagerberg@live.se>
2021-04-15 15:47:48 +0200

    implemented svg image and animation

--- js/main.js
+++ js/main.js
@@ -7,11 +7,23 @@ window.addEventListener('load', function() {
 
     TweenMax.to("#car", 1, {x:20, repeat:10, yoyo:true, ease:Sine.easeInOut, repeat: -1});
 
-    function Tween(){
-        let T = TweenLite.to("#house",0.15,{x:R(-10,10),y:R(-10,10),scale:R(1.1,0.9),rotation:R(-10,10),ease:Sine.easeInOut,onComplete:Tween})
-        };
-        
-        Tween();
-        
-        function R(max,min){return Math.random()*(max-min)+min};
+    TweenMax.to('.window',{scaleX: 1.2, scaleY: 1.2, yoyo:true, duration: 1, repeat: -1});
+    // gsap.from( '#house' , { duration: 2, y: -50, ease: "bounce", delay: .5, repeat: -1 } );
 })
+
+
+const windowColors = ['rgb(241, 250, 123)', 'rgb(255, 255, 255)']
+let currentColor = 0;
+
+changeWindowColor = () => {
+    gsap.to('.window', {
+        fill: windowColors[currentColor],
+        duration: 1,
+        onComplete: changeWindowColor
+    });
+    currentColor++;
+    if (currentColor > windowColors.length -1) {
+        currentColor = 0;
+    }
+}
+changeWindowColor();
